53cad14a0b4cf593bbbf97ea4bc724a483168e84
Suhas Pindiproli <suhpin95@gmail.com>
2023-02-03 19:55:54 -0800

    Init Commit

---
+++ app/src/main/java/com/example/pizzaparty/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.pizzaparty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.RadioGroup;
+import kotlin.math.ceil
+import android.util.Log
+
+const val TAG = "MainActivity"
+const val SLICES_PER_PIZZA = 8
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var numAttendEditText: EditText
+    private lateinit var numPizzasTextView: TextView
+    private lateinit var howHungryRadioGroup: RadioGroup
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG,"On create Invoked")
+        numAttendEditText = findViewById(R.id.num_attend_edit_text)
+        numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
+        howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
+    }
+
+    fun calculateClick(view: View) {
+        val numAttendStr = numAttendEditText.text.toString()
+        val numAttend = numAttendStr.toInt()
+//        val slicesPerPerson = 4
+        // Determine how many slices on average each person will eat
+        val slicesPerPerson = when (howHungryRadioGroup.checkedRadioButtonId) {
+            R.id.light_radio_button -> 2
+            R.id.medium_radio_button -> 3
+            else -> 4
+        }
+        val totalPizzas = ceil(numAttend * slicesPerPerson /
+                SLICES_PER_PIZZA.toDouble()).toInt()
+        numPizzasTextView.text = "Total pizzas: $totalPizzas"
+    }
+}
